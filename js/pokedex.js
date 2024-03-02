01827007d9527a5756153832446978324ca5bd67
viniciusagvaz <agvazvinicius@gmail.com>
2024-03-02 13:32:08 -0300

    loading. pokemon not founded and sound power functions

--- js/pokedex.js
+++ js/pokedex.js
@@ -2,116 +2,130 @@ const pokemonImage = document.querySelector('.pokemon-image');
 const pokemonNumber = document.querySelector('.pokemon-number');
 const pokemonName = document.querySelector('.pokemon-name');
 const pokemonType = document.querySelector('.pokemon-type');
+const pokemonCrie = document.querySelector('.pokemon-crie');
+const buttonSound = document.querySelector('.sound-button');
 
 const form = document.querySelector('.form');
 const input = document.querySelector('.input-search');
 const buttonPrev = document.querySelector('.bttn-prev');
 const buttonNext = document.querySelector('.bttn-next');
 
-let searchPokemon;
+let searchedPokemon;
+let volume = 0.1;
 
 const fetchPokemon = async (pokemon) => {
-  const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
-  );
+	const APIResponse = await fetch(
+		`https://pokeapi.co/api/v2/pokemon/${pokemon}/`
+	);
 
-  if (APIResponse.status === 200) {
-    const data = await APIResponse.json();
-    searchPokemon = data.id;
-    return data;
-  }
+	if (APIResponse.status === 200) {
+		const data = await APIResponse.json();
+		searchedPokemon = data.id;
+		return data;
+	}
 };
 
-const renderData = async (id, name) => {
+const renderData = (id, name) => {
+	if (id) {
+		pokemonNumber.textContent = `#${id}`;
+		pokemonName.textContent = `${name}`;
+	}
 
-  if (id) {
-    pokemonNumber.innerHTML = `#${id}`;
-    pokemonName.innerHTML = `${name}`;
-  }
-
-  if (!id || id > 1025) {
-    pokemonNumber.innerHTML = '';
-    pokemonName.innerHTML = '';
-  }
+	if (!id || id > 1025) {
+		pokemonNumber.textContent = '';
+		pokemonName.textContent = '';
+	}
 };
 
 const renderType = async (pokemon) => {
-  const data = await fetchPokemon(pokemon);
-  const types = data['types'];
-  const typeNames = types.map((slot) => slot['type']['name']);
+	const data = await fetchPokemon(pokemon);
+	const types = data['types'];
+	const typeNames = types.map((slot) => slot['type']['name']);
 
-  for (let type of typeNames) {
-    pokemonType.innerHTML += `
+	for (let type of typeNames) {
+		pokemonType.innerHTML += `
     <img src="img/icons/${type}.svg" alt="pokemon type" class="pokemon-type">
     `;
-  }
+	}
 };
 
-const renderSprites = async (pokemon) => {
-  const sprite = pokemon['sprites']['front_default'];
-  pokemonImage.style.display = 'block';
-  pokemonImage.src = sprite;
-}
+const renderSprites = (pokemon) => {
+	const sprite = pokemon['sprites']['front_default'];
+	pokemonImage.style.display = 'block';
+	pokemonImage.src = sprite;
+};
 
 const renderPokemon = async (pokemon = 1) => {
-  pokemonImage.src = `./img/loading.gif`;
-  pokemonType.innerHTML = '';
-  pokemonName.innerHTML = `Loading...`;
-  pokemonNumber.innerHTML = ``;
+	loadingRender();
+	const data = await fetchPokemon(pokemon);
 
-  const data = await fetchPokemon(pokemon);
-
-  if (data) {
-    renderSprites(data)
-    renderData(formatNumber(pokemon), data.name);
-    renderType(pokemon);
-    playCrieOnRender(data);
-
-    input.value = '';
-    searchPokemon = data.id;
-  } else {
-    pokemonImage.style.display = 'none';
-    pokemonImage.src = '';
-    pokemonType.innerHTML = 'x';
-    renderData('xxxx', 'Not found');
-  }
+	if (data) {
+		renderSprites(data);
+		renderData(formatNumber(pokemon), data.name);
+		renderType(pokemon);
+		playCrieOnRender(data);
+	} else {
+		pokemonNotFounded();
+	}
 };
 
 const playCrieOnRender = (pokemon) => {
-  const pokemonCrie = document.querySelector('.pokemon-crie');
-  let crie = pokemon['cries']['latest'];
-
-  pokemonCrie.src = `${crie}`;
-  pokemonCrie.volume = 0.2;
+	let crie = pokemon['cries']['latest'];
+	pokemonCrie.src = `${crie}`;
+	pokemonCrie.volume = volume;
 };
 
 const formatNumber = (number) => {
-  return number < 10
-    ? `000${number}`
-    : `${number}` && number < 100
-      ? `00${number}`
-      : `${number}` && number < 1000
-        ? `0${number}`
-        : `${number}`;
+	return number < 10
+		? `000${number}`
+		: `${number}` && number < 100
+		? `00${number}`
+		: `${number}` && number < 1000
+		? `0${number}`
+		: `${number}`;
+};
+
+const loadingRender = () => {
+	pokemonImage.src = `./img/loading.gif`;
+	pokemonType.textContent = '';
+	pokemonName.textContent = `Loading...`;
+	pokemonNumber.textContent = `#xxxx`;
+};
+
+const pokemonNotFounded = () => {
+	pokemonImage.style.display = 'none';
+	pokemonImage.src = '';
+	pokemonType.textContent = 'x';
+	renderData('xxxx', 'Not found');
 };
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const inputValue = input.value.toLowerCase().trim();
+	event.preventDefault();
+	const inputValue = input.value.toLowerCase().trim();
 
-  renderPokemon(inputValue);
+	input.value = '';
+	renderPokemon(inputValue);
 });
 
 buttonPrev.addEventListener('click', () => {
-  if (searchPokemon > 1) {
-    searchPokemon--;
-    renderPokemon(searchPokemon);
-  }
+	if (searchedPokemon > 1) {
+		searchedPokemon--;
+		renderPokemon(searchedPokemon);
+	}
 });
 
 buttonNext.addEventListener('click', () => {
-  searchPokemon++;
-  renderPokemon(searchPokemon);
+	searchedPokemon++;
+	renderPokemon(searchedPokemon);
+});
+
+buttonSound.addEventListener('click', () => {
+	const status = document.querySelector('.sound-status');
+	volume = !volume;
+	volume ? (volume = 0.1) : volume;
+	volume
+		? (status.style.backgroundColor = '#9ffe58')
+		: (status.style.backgroundColor = '#222');
 });
 
 renderPokemon();
